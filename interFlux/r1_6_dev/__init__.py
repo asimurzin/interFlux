3375368eb30ac49878f6bd58bc6a90b8a7851855
Andrey Simurzin <asimurzin@gmail.com>
2011-04-19 09:23:29 +0400

    to remove "-test" feature

--- interFlux/r1_6_dev/__init__.py
+++ interFlux/r1_6_dev/__init__.py
@@ -387,17 +387,19 @@ def main_standalone( argc, argv ):
 
 
 #--------------------------------------------------------------------------------------
-import sys, os
 from Foam import FOAM_BRANCH_VERSION
 if FOAM_BRANCH_VERSION( "dev", ">=", "010600" ):
    if __name__ == "__main__" :
+      import sys, os
       argv = sys.argv
-      if len( argv ) > 1 and argv[ 1 ] == "-test":
-         argv = None
-         test_dir= os.path.join( os.environ[ "PYFOAM_TESTING_DIR" ],'cases', 'propogated', 'r1.6-dev', 'multiphase','interFoam', 'laminar', 'damBreak' )
-         argv = [ __file__, "-case", test_dir ]
-         pass
       os._exit( main_standalone( len( argv ), argv ) )
+      pass
+   pass
+else:
+   from Foam.OpenFOAM import ext_Info
+   ext_Info()<< "\nTo use this solver, It is necessary to SWIG OpenFoam1.6-ext or higher \n "     
+   pass
+
 
 
 #--------------------------------------------------------------------------------------
