03307b0f3760b0ff30ef2baa7dcc885bb55e2e39
Andrey Simurzin <asimurzin@gmail.com>
2012-06-15 22:00:36 +0400

    Import solver module one time only.

--- interFlux/__init__.py
+++ interFlux/__init__.py
@@ -38,19 +38,19 @@ if FOAM_BRANCH_VERSION( "dev", ">=", "010600" ):
 
 
 #--------------------------------------------------------------------------------------
-if FOAM_REF_VERSION( ">=", "010700" ):
+if FOAM_REF_VERSION( "==", "010700" ) or FOAM_REF_VERSION( "==", "010701" ):
     from interFlux.r1_7_0 import *
     pass
 
 
 #--------------------------------------------------------------------------------------
-if FOAM_VERSION( ">=", "020000" ):
+if FOAM_VERSION( "==", "020000" ) or FOAM_REF_VERSION( "==", "020001" ):
     from interFlux.r2_0_0 import *
     pass
 
 
 #--------------------------------------------------------------------------------------
-if FOAM_VERSION( ">=", "020100" ):
+if FOAM_VERSION( "==", "020100" ) :
     from interFlux.r2_1_0 import *
     pass
 
